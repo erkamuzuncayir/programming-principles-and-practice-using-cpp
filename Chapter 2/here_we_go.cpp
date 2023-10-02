d68e64429e995099a71579c91d6cdc1f2a4e0194
Erkam Uzuncayir <erkamuzuncayir@gmail.com>
2023-10-02 23:52:25 +0300

    Add chapter 2, exercise 1 to repository

---
+++ Chapter 2/here_we_go.cpp
@@ -0,0 +1,14 @@
+//
+// Created by erkam on 02.10.2023.
+//
+// Chapter 2 - Exercise 1
+
+#include "./std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, programming!\n";
+    cout << "Here we go!\n";
+    keep_window_open();
+    return 0;
+}
