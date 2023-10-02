6add3b76cd0bc971040b4a3b212b575cde5cbfba
Erkam Uzuncayir <erkamuzuncayir@gmail.com>
2023-10-02 23:52:17 +0300

    Add chapter 2, drill 1 to repository

---
+++ Chapter 2/hello_world.cpp
@@ -0,0 +1,13 @@
+//
+// Created by erkam on 02.10.2023.
+//
+// Chapter 2 - Drill 1
+
+#include "./std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, World!\n";
+    keep_window_open(); // wait for a character to be entered.
+    return 0;
+}
